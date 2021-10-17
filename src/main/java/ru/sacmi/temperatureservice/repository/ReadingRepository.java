bea794370ded578ea839cd094fa7b49087701a31
Igor Anufriev <sacmi@pm.me>
2021-10-17 17:22:28 +0700

    Адаптация для JDK11

--- src/main/java/ru/sacmi/temperatureservice/repository/ReadingRepository.java
+++ src/main/java/ru/sacmi/temperatureservice/repository/ReadingRepository.java
@@ -12,12 +12,11 @@ public interface ReadingRepository extends JpaRepository<ReadingEntity, Long> {
 
     Collection<ReadingEntity> findBySensorOrderById(SensorEntity sensor);
 
-    @Query("""
-        SELECT r FROM ReadingEntity r
-        WHERE r.sensor = :sensor
-        AND (CAST(:fromTime AS date) IS NULL OR r.timestamp >= :fromTime)
-        AND (CAST(:toTime AS date) IS NULL OR r.timestamp <= :toTime)
-        ORDER BY r.id""")
+    @Query("SELECT r FROM ReadingEntity r\n" +
+           "WHERE r.sensor = :sensor\n" +
+           "AND (CAST(:fromTime AS date) IS NULL OR r.timestamp >= :fromTime)\n" +
+           "AND (CAST(:toTime AS date) IS NULL OR r.timestamp <= :toTime)\n" +
+           "ORDER BY r.id")
     Collection<ReadingEntity> findByFilter(
         @NotNull SensorEntity sensor, Instant fromTime, Instant toTime);
 }
--- src/test/java/ru/sacmi/temperatureservice/TemperatureServiceApplicationTests.java
+++ src/test/java/ru/sacmi/temperatureservice/TemperatureServiceApplicationTests.java
@@ -6,8 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TemperatureServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
 }
